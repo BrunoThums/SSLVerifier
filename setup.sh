09dad9033159e43c235508c250c593341ccf6775
Bruno Brill Thums <bruno.thums@universo.univates.br>
2023-08-30 11:04:13 -0300

    Colored setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-echo "Setup for SSLVerifier commencing!"
+echo "\e[36mSetup for SSLVerifier commencing\e[0m!"
 
 # Verify if command was used with sudo
 if [ "$(id -u)" -ne 0 ]; then
-    echo "Execute again with sudo, please!"
+    echo "\e[36mExecute again with sudo, please!\e[0m"
     exit 1
 fi
 
 # Verify if sslscan is installed. Else, install
 if ! command -v sslscan &> /dev/null; then
-    echo "Installing sslscan..."
+    echo "\e[36mInstalling sslscan...\e[0m"
     apt-get update
     apt-get install -y sslscan
-    echo "Installed sslscan."
+    echo "\e[36mInstalled sslscan.\e[0m"
 else
-    echo "sslscan is already installed."
+    echo "\e[36msslscan is already installed.\e[0m"
 fi
 
 # Download sslverifier.sh
@@ -24,9 +24,9 @@ wget "https://raw.githubusercontent.com/BrunoThums/SSLVerifier/main/sslverifier.
 chmod +x "sslverifier.sh"
 
 # Move the entire directory to /usr/local/bin
-echo "Moving script to path"
+echo "\e[36mMoving script to path\e[0m"
 mv sslverifier.sh /usr/bin/sslverifier
-echo "Done!"
-echo "You can now use sslverifier anywhere :)"
-echo "Goodbye!"
+echo "\e[36mDone!\e[0m"
+echo "\e[36mYou can now use sslverifier anywhere :)\e[0m"
+echo "\e[36mGoodbye!\e[0m"
 rm setup.sh
