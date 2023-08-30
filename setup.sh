6853a0c3983abd3d527255ed361a89090e3f7104
Bruno Brill Thums <bruno.thums@universo.univates.br>
2023-08-30 11:12:29 -0300

    Colored setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-echo -e "\e[36m Setup for SSLVerifier commencing \e[0m!"
+echo -e "\033[36m Setup for SSLVerifier commencing \033[0m!"
 
 # Verify if command was used with sudo
 if [ "$(id -u)" -ne 0 ]; then
-    echo -e "\e[36m Execute again with sudo, please! \e[0m"
+    echo -e "\033[36m Execute again with sudo, please! \033[0m"
     exit 1
 fi
 
 # Verify if sslscan is installed. Else, install
 if ! command -v sslscan &> /dev/null; then
-    echo -e "\e[36m Installing sslscan... \e[0m"
+    echo -e "\033[36m Installing sslscan... \033[0m"
     apt-get update
     apt-get install -y sslscan
-    echo -e "\e[36m Installed sslscan. \e[0m"
+    echo -e "\033[36m Installed sslscan. \033[0m"
 else
-    echo -e "\e[36m sslscan is already installed. \e[0m"
+    echo -e "\033[36m sslscan is already installed. \033[0m"
 fi
 
 # Download sslverifier.sh
@@ -24,9 +24,9 @@ wget "https://raw.githubusercontent.com/BrunoThums/SSLVerifier/main/sslverifier.
 chmod +x "sslverifier.sh"
 
 # Move the entire directory to /usr/local/bin
-echo -e "\e[36m Moving script to path \e[0m"
+echo -e "\033[36m Moving script to path \033[0m"
 mv sslverifier.sh /usr/bin/sslverifier
-echo -e "\e[36m Done! \e[0m"
-echo -e "\e[36m You can now use sslverifier anywhere :) \e[0m"
-echo -e "\e[36m Goodbye! \e[0m"
+echo -e "\033[36m Done! \033[0m"
+echo -e "\033[36m You can now use sslverifier anywhere :) \033[0m"
+echo -e "\033[36m Goodbye! \033[0m"
 rm setup.sh
